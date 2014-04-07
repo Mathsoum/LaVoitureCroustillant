4bb749b9be3a3862cb6306bbdaa62a63aaf4b297
hordesk <mathias.faure.31@gmail.com>
2014-04-07 17:31:45 +0200

    US4 Terminated

--- src/main/java/applicationSimulateur/DessinVoiture.java
+++ src/main/java/applicationSimulateur/DessinVoiture.java
@@ -1,19 +1,22 @@
 package applicationSimulateur;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Polygon;
 
 import javax.swing.JFrame;
 
 public class DessinVoiture extends JFrame {
 
-	public static final int TailleFenetreEnPixels = 500;
+	public static final int TAILLE_FENETRE_EN_PIXEL = 500;
+	public static final int POSITION_Y = 200;
 	
 	private int xPixelVoiture;
 
 	public DessinVoiture() {
 		super();
 		this.setTitle("Simulateur de Voiture");
-		this.setSize(TailleFenetreEnPixels, TailleFenetreEnPixels);
+		this.setSize(TAILLE_FENETRE_EN_PIXEL, TAILLE_FENETRE_EN_PIXEL);
 		this.setVisible(true);
 		this.xPixelVoiture = 0;
 	}
@@ -26,7 +29,23 @@ public class DessinVoiture extends JFrame {
 	}
 
 	public void dessinerVoiture(int xPixelVoiture, Graphics graphics) {
-		graphics.fillRect(xPixelVoiture, 200, 40, 20);
+		
+		
+		graphics.setColor(Color.RED);
+		Polygon rectangle = new Polygon();
+		rectangle.addPoint(xPixelVoiture - 20, POSITION_Y - 10);
+		rectangle.addPoint(xPixelVoiture - 20, POSITION_Y + 10);
+		rectangle.addPoint(xPixelVoiture + 20, POSITION_Y + 10);
+		rectangle.addPoint(xPixelVoiture + 20, POSITION_Y - 10);
+		graphics.fillPolygon(rectangle);
+		
+		graphics.setColor(Color.WHITE);
+		Polygon triangle = new Polygon();
+		triangle.addPoint(xPixelVoiture, POSITION_Y - 10);
+		triangle.addPoint(xPixelVoiture, POSITION_Y + 10);
+		triangle.addPoint(xPixelVoiture +20, POSITION_Y);
+		graphics.fillPolygon(triangle);
+		graphics.setColor(Color.BLACK);
 
 	}
 
