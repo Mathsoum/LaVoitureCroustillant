6897731f22f1103c3adb0e6346517134e92f06b6
hordesk <mathias.faure.31@gmail.com>
2014-04-14 16:23:02 +0200

    US8 : Ajout de la seconde dimension

--- src/main/java/domaineConduite/Voiture.java
+++ src/main/java/domaineConduite/Voiture.java
@@ -6,16 +6,18 @@ public class Voiture extends Observable {
 	private static final float TICK_ANGLE_ROTATION = 30;
 	private float angleDirection;
 	private int coordXEnMetres;
+	private int coordYEnMetres;
 	private int vitesseMetreParSecondes;
 
 	public static final int largeurDomaine = 1000;
 
-	public Voiture(int coordXEnMetres) {
-		this(coordXEnMetres, 0);
+	public Voiture(int coordXEnMetres, int coordYEnMetres) {
+		this(coordXEnMetres, coordYEnMetres, 0);
 	}
 
-	public Voiture(int coordXEnMetres, int vitesseMetreParSecondes) {
+	public Voiture(int coordXEnMetres, int coordYEnMetres, int vitesseMetreParSecondes) {
 		this.coordXEnMetres = coordXEnMetres;
+		this.coordYEnMetres = coordYEnMetres;
 		this.vitesseMetreParSecondes = vitesseMetreParSecondes;
 		this.angleDirection = 0;
 	}
@@ -24,12 +26,17 @@ public class Voiture extends Observable {
 		return coordXEnMetres;
 	}
 
+	public int getCoordYEnMetres() {
+		return coordYEnMetres;
+	}
+
 	public int getVitesseMetreParSecondes() {
 		return vitesseMetreParSecondes;
 	}
 
 	public void avancerEnFonctionDeLaVitesse() {
-		coordXEnMetres += vitesseMetreParSecondes;
+		coordXEnMetres += vitesseMetreParSecondes * Math.cos(Math.toRadians(angleDirection));
+		coordYEnMetres += vitesseMetreParSecondes * Math.sin(Math.toRadians(angleDirection));
 		notificationObservateurs();
 	}
 
@@ -43,9 +50,9 @@ public class Voiture extends Observable {
 	}
 
 	public void tourneADroite() {
-		angleDirection += TICK_ANGLE_ROTATION;
+		angleDirection -= TICK_ANGLE_ROTATION;
 	}
 	public void tourneAGauche() {
-		angleDirection -= TICK_ANGLE_ROTATION;
+		angleDirection += TICK_ANGLE_ROTATION;
 	}
 }
