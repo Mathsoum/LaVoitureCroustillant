e337f0c940e2261d9d499f7ad3058931b5e191ac
hordesk <mathias.faure.31@gmail.com>
2014-04-21 14:17:35 +0200

    Refactor tests

--- src/test/java/tests/TestVoiture.java
+++ src/test/java/tests/TestVoiture.java
@@ -5,155 +5,135 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import domaineConduite.Voiture;
-
-
+import domaineconduite.Voiture;
 
 public class TestVoiture {
-	
-	@Test
-	public void testGetX(){
-		Voiture voiture = new Voiture(20, 20) ;
-		
-		int xVoiture = voiture.getCoordXEnMetres() ;
-		
-		assertEquals(20, xVoiture);
-		
+	Voiture voiture;
+
+	@Before
+	public void setUp() {
+		voiture = new Voiture( 100, 200, 50 );
 	}
-	
+
+	@Test
+	public void testGetX() {
+		assertEquals( 100, voiture.getCoordXEnMetres() );
+	}
+
+	@Test
+	public void testGetY() {
+		assertEquals( 200, voiture.getCoordYEnMetres() );
+	}
+
 	@Test
 	public void testVitesseX() {
-		Voiture voiture = new Voiture(20, 30, 150);
-		
-		assertEquals(150,voiture.getVitesseMetreParSecondes());
+		assertEquals( 50,voiture.getVitesseMetreParSecondes() );
 	}
-	
+
 	@Test
 	public void testAccelerer() {
-		Voiture voiture = new Voiture(20, 30, 150);
-		
-		assertEquals(150,voiture.getVitesseMetreParSecondes());
+		assertEquals( 50,voiture.getVitesseMetreParSecondes() );
+
 		voiture.accelerer();		
-		assertEquals(160,voiture.getVitesseMetreParSecondes());
+
+		assertEquals( 60,voiture.getVitesseMetreParSecondes() );
 	}
 	@Test
 	public void testRalentir() {
-		Voiture voiture = new Voiture(20, 30, 150);
-		
-		assertEquals(150,voiture.getVitesseMetreParSecondes());
+		assertEquals( 50,voiture.getVitesseMetreParSecondes() );
+
 		voiture.ralentir();		
-		assertEquals(140,voiture.getVitesseMetreParSecondes());
+
+		assertEquals( 40,voiture.getVitesseMetreParSecondes() );
 	}
-	
+
 	@Test
 	public void testMiseAjourPositionEnFonctionVitesse() {
-		Voiture voiture = new Voiture(20, 30, 100);
 		voiture.avancerEnFonctionDeLaVitesse();
-		assertEquals(120, voiture.getCoordXEnMetres());
+
+		assertEquals( 150, voiture.getCoordXEnMetres() );
 	}
 
 	@Test
 	public void testAngleDepart() {
-		Voiture voiture = new Voiture(20, 30, 100);
-		assertEquals(0, voiture.getAngle(), .05);
+		assertEquals( 0, voiture.getAngle(), .05 );
 	}
 
 	@Test
 	public void testAngleApresVirageAGauche() {
-		Voiture voiture = new Voiture(20, 30);
 		voiture.tourneAGauche();
-		assertEquals(-30, voiture.getAngle(), .05);
+
+		assertEquals( -30, voiture.getAngle(), .05 );
 	}
-	
+
 	@Test
 	public void testAngleApresVirageADroite() {
-		Voiture voiture = new Voiture(20, 30);
 		voiture.tourneADroite();
-		assertEquals(30, voiture.getAngle(), .05);
+
+		assertEquals( 30, voiture.getAngle(), .05 );
 	}
-	
+
 	@Test
 	public void testAngleApresPlusieursVirages() {
-		Voiture voiture = new Voiture(20, 30);
 		voiture.tourneADroite();
 		voiture.tourneADroite();
 		voiture.tourneAGauche();
 		voiture.tourneAGauche();
 		voiture.tourneAGauche();
-		assertEquals(-30, voiture.getAngle(), .05);
+
+		assertEquals( -30, voiture.getAngle(), .05 );
 	}
-	
+
 	@Test
-	public void testGetY(){
-		Voiture voiture = new Voiture(20, 30) ;
-		
-		int yVoiture = voiture.getCoordYEnMetres() ;
-		
-		assertEquals(30, yVoiture);
-		
-	}
-	
-	@Test
-	public void testMiseAJourPositionSansRotation(){
-		Voiture voiture = new Voiture(100, 100, 5) ;
-		
+	public void testMiseAJourPositionSansRotation() {
 		voiture.avancerEnFonctionDeLaVitesse();
 
-		assertEquals(105, voiture.getCoordXEnMetres());
-		assertEquals(100, voiture.getCoordYEnMetres());
+		assertEquals( 150, voiture.getCoordXEnMetres() );
+		assertEquals( 200, voiture.getCoordYEnMetres() );
 	}
-	
-	@Test
-	public void testMiseAJourPositionRotationGauche(){
-		Voiture voiture = new Voiture(100, 100, 5) ;
 
+	@Test
+	public void testMiseAJourPositionRotationGauche() {
 		voiture.tourneADroite();
 		voiture.tourneADroite();
 		voiture.tourneADroite();
-		
+
 		voiture.avancerEnFonctionDeLaVitesse();
 
-		assertEquals(100, voiture.getCoordXEnMetres());
-		assertEquals(105, voiture.getCoordYEnMetres());
-		
+		assertEquals( 100, voiture.getCoordXEnMetres() );
+		assertEquals( 250, voiture.getCoordYEnMetres() );
 	}
-	
-	@Test
-	public void testMiseAJourPositionRotationDroite(){
-		Voiture voiture = new Voiture(100, 100, 5) ;
 
+	@Test
+	public void testMiseAJourPositionRotationDroite() {
 		voiture.tourneAGauche();
 		voiture.tourneAGauche();
 		voiture.tourneAGauche();
-		
+
 		voiture.avancerEnFonctionDeLaVitesse();
 
-		assertEquals(100, voiture.getCoordXEnMetres());
-		assertEquals(95, voiture.getCoordYEnMetres());
-		
+		assertEquals( 100, voiture.getCoordXEnMetres() );
+		assertEquals( 150, voiture.getCoordYEnMetres() );
 	}
-	
+
 	@Test
-	public void testMiseAJourPositionRotationRetour(){
-		Voiture voiture = new Voiture(100, 100, 5) ;
+	public void testMiseAJourPositionRotationRetour() {
+		voiture.tourneADroite();
+		voiture.tourneADroite();
+		voiture.tourneADroite();
+		voiture.tourneADroite();
+		voiture.tourneADroite();
+		voiture.tourneADroite();
 
-		voiture.tourneADroite();
-		voiture.tourneADroite();
-		voiture.tourneADroite();
-		voiture.tourneADroite();
-		voiture.tourneADroite();
-		voiture.tourneADroite();
-		
 		voiture.avancerEnFonctionDeLaVitesse();
 
-		assertEquals(95, voiture.getCoordXEnMetres());
-		assertEquals(100, voiture.getCoordYEnMetres());
-		
+		assertEquals( 50, voiture.getCoordXEnMetres() );
+		assertEquals( 200, voiture.getCoordYEnMetres() );
 	}
-	
+
 	@Test
 	public void testLimiteBassesX() {
-		Voiture voiture = new Voiture(0, 500, 5) ;
+		voiture = new Voiture( 0, 500, 5 ) ;
 
 		voiture.tourneADroite();
 		voiture.tourneADroite();
@@ -161,46 +141,44 @@ public class TestVoiture {
 		voiture.tourneADroite();
 		voiture.tourneADroite();
 		voiture.tourneADroite();
-		
+
 		voiture.avancerEnFonctionDeLaVitesse();
 
-		assertEquals(0, voiture.getCoordXEnMetres());
-		
+		assertEquals( 0, voiture.getCoordXEnMetres() );
 	}
-	
+
 	@Test
 	public void testLimiteBassesY() {
-		Voiture voiture = new Voiture(500, 0, 5) ;
+		voiture = new Voiture( 500, 0, 5 ) ;
 
 		voiture.tourneAGauche();
 		voiture.tourneAGauche();
 		voiture.tourneAGauche();
-		
+
 		voiture.avancerEnFonctionDeLaVitesse();
 
-		assertEquals(0, voiture.getCoordYEnMetres());
-		
+		assertEquals( 0, voiture.getCoordYEnMetres() );
 	}
-	
+
 	@Test
 	public void testLimiteHautesX() {
-		Voiture voiture = new Voiture(1000, 500, 5) ;
-		
+		voiture = new Voiture( 1000, 500, 5 ) ;
+
 		voiture.avancerEnFonctionDeLaVitesse();
 
-		assertEquals(1000, voiture.getCoordXEnMetres());
+		assertEquals( 1000, voiture.getCoordXEnMetres() );
 	}
-	
+
 	@Test
 	public void testLimiteHautesY() {
-		Voiture voiture = new Voiture(500, 1000, 5) ;
+		voiture = new Voiture( 500, 1000, 5 ) ;
 
 		voiture.tourneADroite();
 		voiture.tourneADroite();
 		voiture.tourneADroite();
-		
+
 		voiture.avancerEnFonctionDeLaVitesse();
 
-		assertEquals(1000, voiture.getCoordYEnMetres());
+		assertEquals( 1000, voiture.getCoordYEnMetres() );
 	}
 }
